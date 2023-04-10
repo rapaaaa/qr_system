79ae88b3074fe9d4aa8adc1f5aec5e388c3bd640
rapaaaa1 <rafaelclaveria.rc@gmail.com>
2023-04-10 15:49:06 +0800

    Update
    doctor schedule update modal title rename

--- view/modals/update_doctor_schedule_modal.php
+++ view/modals/update_doctor_schedule_modal.php
@@ -6,7 +6,7 @@
 	            <div class="modal-header">
 	                <h5 class="modal-title" id="exampleModalLabel">
 	                    <i class="fas fa-edit"></i>
-	                    <span class="text">Update medicine/vaccine</span></h5>
+	                    <span class="text">Update doctor schedule</span></h5>
 	                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
 	                    <span aria-hidden="true">×</span>
 	                </button>
