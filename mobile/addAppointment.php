4fe2e5738f17d0e10da5ab49b8e92662cdc63790
jepjepj <58154782+jepjepj@users.noreply.github.com>
2023-04-24 15:29:12 +0800

    Update addAppointment.php

--- mobile/addAppointment.php
+++ mobile/addAppointment.php
@@ -12,18 +12,23 @@ $data = json_decode(file_get_contents("php://input"));
 if(isset($data->user_id) && $data->user_id > 0){
 
     $service_id = $mysqli_connect->real_escape_string($data->service_id);
-    $app_time = getCurrentDate(); //$mysqli_connect->real_escape_string($data->app_time);
+    $app_time = date("Y-m-d", strtotime($mysqli_connect->real_escape_string($data->app_time)));
     $description = $mysqli_connect->real_escape_string($data->description);
     $patient_id = $mysqli_connect->real_escape_string($data->user_id);
 
-
-    $date = getCurrentDate();
-
-    $sql= $mysqli_connect->query("INSERT INTO `appointments`(`service_id`, `app_time`, `description`,`patient_id`, date_added) VALUES ('$service_id', '$app_time', '$description','$patient_id', '$app_time')");
+    $fetch = $mysqli->query("SELECT * FROM doctor_schedule WHERE date='$app_time'") or die(mysqli_error());
+    $row = $fetch->fetch_array();
+    $max_residents = $row['max_residents'];
+    
+    if($max_residents > 0){
+        $sql= $mysqli_connect->query("INSERT INTO `appointments`(`service_id`, `app_time`, `description`,`patient_id`, date_added) VALUES ('$service_id', '$app_time', '$description','$patient_id', '$app_time')");
                 
-    if($sql){
-        echo 1;
+        if($sql){
+            echo 1;
+        }else{
+            echo 0;
+        }
     }else{
-        echo 0;
+        echo 2;
     }
 }
